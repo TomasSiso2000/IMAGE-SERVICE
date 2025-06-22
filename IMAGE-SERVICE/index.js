213c98e161895183d46fc5c5baf5bfeb8ac39939
TomasSiso2000 <112671894+TomasSiso2000@users.noreply.github.com>
2025-06-21 21:12:46 -0300

    Add files via upload

---
+++ IMAGE-SERVICE/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { graphqlUploadExpress } = require('graphql-upload');
+const path = require('path');
+const typeDefs = require('./src/graphql/typeDefs/schema'); 
+const resolvers = require('./src/graphql/resolvers/imageResolvers'); 
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+app.use('/uploads', express.static(path.join(__dirname, 'temp')));
+app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 1 }));
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+app.listen(PORT, () => {
+  console.log('Servidor GraphQL en http://localhost:' + PORT + '/graphql');
+});
---
+++ IMAGE-SERVICE/src/graphql/resolvers/imageResolvers.js
@@ -0,0 +1,43 @@
+const cloudinary = require("../../config/cloudinary");
+const extractPublicId = require("../../util/extractPublicId");
+const { GraphQLUpload } = require('graphql-upload');
+
+const resolvers = {
+  Upload: GraphQLUpload,
+
+  Mutation: {
+    uploadImage: async (_, { file }) => {
+      const { createReadStream } = await file;
+      const stream = createReadStream();
+
+      return new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          { folder: 'uploads' },
+          (error, result) => {
+            if (error) return reject(error);
+
+            resolve({
+              url: result.secure_url
+            });
+          }
+        );
+        stream.pipe(uploadStream);
+      });
+    },
+
+    deleteImage: async (_, { url }) => {
+      const publicId = extractPublicId(url);
+      if (!publicId) return false;
+
+      try {
+        const result = await cloudinary.uploader.destroy(publicId);
+        return result.result === 'ok';
+      } catch (error) {
+        console.error('Error eliminando imagen:', error);
+        return false;
+      }
+    }
+  }
+};
+
+module.exports = resolvers;
